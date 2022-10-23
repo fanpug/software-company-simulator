dd06b9d48ea04deeb9c60cf620fecb972abf30fb
Humberto Navarro <a320669@uach.mx>
2022-10-23 00:07:36 -0600

    Se crea el concepto del juego

---
+++ main.cpp
@@ -0,0 +1,91 @@
+/***********************************************
+ * Hecho por:
+ * Humberto Alejandro Navarro Andujo - 320669
+ * Martín Francisco Ortiz Guerrero - 329783
+ * Raúl Alejandro Díaz Gutiérrez - 329782
+ * Lesly Alejandra Santarriaga Olivas - 345525
+***********************************************/
+
+// Librerias
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+
+// Declaración de funciones
+void gotoXY(int, int); // declaring the goto function which is set up below
+void marco();
+void pantallaInicio();
+void menuPrincipal();
+
+// Función main
+int main()
+{
+	pantallaInicio();
+	system("pause>nul");
+    system("CLS");
+
+	menuPrincipal();
+	system("pause>nul");
+    system("CLS");
+
+    return 0;
+}
+
+// Función gotoXY, nos permite escribir en partes especificas de la consola dandole coordenadas
+void gotoXY(int x, int y){
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Función marco, imprime el marco que "delimita" el programa (100x25)
+void marco(){
+	gotoXY(0,0);
+	for (int i = 0; i < 100; i++){
+		cout << "#";
+	}
+	
+	for (int i = 0; i < 25; i++){
+		gotoXY(0,i);
+		cout << "#";
+	}
+
+	for (int i = 0; i < 25; i++){
+		gotoXY(100,i);
+		cout << "#";
+	}
+
+	gotoXY(0,25);
+	for (int i = 0; i < 101; i++){
+		cout << "#";
+	}
+}
+
+// Función pantallaInicio, aqui se imprime la pantalla de inicio del programa
+void pantallaInicio(){
+	marco();
+	gotoXY(28, 8);
+	cout << "Bienvenido a software company simulator 2022";
+	gotoXY(32, 18);
+	cout << "Presiona enter para continuar...";
+}
+
+// Función menuPrincipal, aqui se imprime el menu principal del juego
+void menuPrincipal(){
+	marco();
+	gotoXY(28, 8);
+	cout << "En esta pantalla hay un menu de opciones para";
+	gotoXY(28, 9);
+	cout << "los creditos y para iniciar el juego.";
+	gotoXY(28, 15);
+	cout << "... o esa es la idea...";
+	/*
+	* podemos poner varias cosas en una pantalla o
+	* tener varias pantallas y tener separado todo
+	* con una variable global para el contador de
+	* la puntuacion y cosas asi
+	*/
+
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# software-company-simulator
+Repositorio para juego de ingenieria de software 2
+
+# creditos
+Hecho por:
+ * Humberto Alejandro Navarro Andujo - 320669
+ * Martín Francisco Ortiz Guerrero - 329783
+ * Raúl Alejandro Díaz Gutiérrez - 329782
+ * Lesly Alejandra Santarriaga Olivas - 345525
