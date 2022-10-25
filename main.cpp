0dfe7b616c033274b2430bccdd7bffdd041772a2
Humberto Navarro <a320669@uach.mx>
2022-10-24 20:38:16 -0600

    Se agrega contador global, definiciones de posicionamiento de preguntas, funciones de prueba de pregunta y controles de jugador

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@
  * Raúl Alejandro Díaz Gutiérrez - 329782
  * Lesly Alejandra Santarriaga Olivas - 345525
 ***********************************************/
-
 // Librerias
 #include<iostream>
 #include<windows.h>
+
+// Definiciones
+#define PREGUNTA1POSX 10
+#define PREGUNTA2POSX 60
+#define PREGUNTAPOSY 18
+
+int contador = 0;
 
 using namespace std;
 
@@ -16,8 +22,10 @@ using namespace std;
 void gotoXY(int, int); // declaring the goto function which is set up below
 void marco();
 void pantallaInicio();
-void menuPrincipal();
+void introduccion();
+int control();
 void test();
+void ejemploPregunta();
 
 // Función main
 int main()
@@ -26,11 +34,13 @@ int main()
     system("pause>nul");
     system("CLS");
 
-    menuPrincipal();
+    introduccion();
     system("pause>nul");
     system("CLS");
 
-    test();
+	ejemploPregunta();
+    system("pause>nul");
+    system("CLS");
 
     return 0;
 }
@@ -77,52 +87,128 @@ void pantallaInicio()
 {
     marco();
     gotoXY(28, 8);
-    cout << "Bienvenido a software company simulator 2022";
-    gotoXY(32, 18);
+    cout << "Bienvenido a Software Company Simulator 2022";
+
+	gotoXY(22, 10);
+    cout << "En este juego tú eres el lider de una empresa de software";
+	gotoXY(22, 11);
+	cout << "y, como lider, tú tomas las decisiones de la compañia.";
+
+    gotoXY(32, 19);
     cout << "Presiona enter para continuar...";
 }
 
-// Función menuPrincipal, aqui se imprime el menu principal del juego
-void menuPrincipal()
+// Función introduccion, aqui se imprime una descripcion de la empresa que manejas como jugador y que decisiones tendras que tomar
+void introduccion()
 {
     marco();
     gotoXY(28, 8);
-    cout << "En esta pantalla hay un menu de opciones para";
+    cout << "En esta pantalla da una pequeña introduccion";
     gotoXY(28, 9);
-    cout << "los creditos y para iniciar el juego.";
+    cout << "a como se juega el juego y que tipo de decisiones";
+	gotoXY(28, 9);
+    cout << "debera tomar el jugador como \"lider\" de la empresa";
     gotoXY(28, 15);
     cout << "... o esa es la idea...";
-    /*
-    * podemos poner varias cosas en una pantalla o
-    * tener varias pantallas y tener separado todo
-    * con una variable global para el contador de
-    * la puntuacion y cosas asi
-    */
+}
 
+// Función control, esta función retorna la posicion en X de la flecha que representa la eleccion de una opcion. Si retorna 1 entonces escogió la opción de la izquierda, si retorna 2 entonces escogió la opción de la derecha
+int control()
+{
+	//opcion = 1 es la opcion de la izquierda y dos es la opcion de la derecha
+	int opcion = 1;
+    gotoXY(PREGUNTA1POSX-3, PREGUNTAPOSY);
+    cout << "->";
+    while(true)
+    {
+        system("pause>nul");
+        if(GetAsyncKeyState(VK_LEFT))
+        {
+			opcion = 1;
+			gotoXY(PREGUNTA2POSX-3, PREGUNTAPOSY);
+			cout << "  ";
+            gotoXY(PREGUNTA1POSX-3, PREGUNTAPOSY);
+            cout << "->";
+            continue;
+        }
+        if(GetAsyncKeyState(VK_RIGHT))
+        {
+			opcion = 2;
+			gotoXY(PREGUNTA1POSX-3, PREGUNTAPOSY);
+			cout << "  ";
+            gotoXY(PREGUNTA2POSX-3, PREGUNTAPOSY);
+            cout << "->";
+            continue;
+        }
+		if(GetAsyncKeyState(VK_RETURN))
+        {
+			break;
+        }
+    }
+	return opcion;
 }
 
 // funcion test, para probar los controles del jugador
 void test()
 {
+	marco();
     bool choose = true;
     gotoXY(3, 13);
     cout << "->";
     while(choose == true)
     {
         system("pause>nul");
-        system("CLS");
         if(GetAsyncKeyState(VK_LEFT))
         {
+			gotoXY(6, 13);
+			cout << "  ";
             gotoXY(3, 13);
             cout << "->";
             continue;
         }
         if(GetAsyncKeyState(VK_RIGHT))
         {
+			gotoXY(3, 13);
+			cout << "  ";
             gotoXY(6, 13);
             cout << "->";
             continue;
         }
 
     }
+}
+
+/************************************************
+ * SECCION DE FUNCIONES DE PREGUNTAS
+ * Cada funcion es una pregunta
+ * La funcion de control esta escrita para que, en cada pantalla, las opciones que el jugador puede escoger deben empezar a ser escritas en las coordenadas (10,18) y (60,18) para que se alinien con las flechas
+************************************************/
+
+void ejemploPregunta(){
+	marco();
+
+	//Pregunta
+    gotoXY(15, 8);
+    cout << "Si hubiera una disputa entre dos trabajadores sobre como resolver";
+    gotoXY(15, 9);
+    cout << "un problema del programa principal, como la resolverias?";
+
+	//Opciones
+    gotoXY(PREGUNTA1POSX, PREGUNTAPOSY);
+    cout << "Correrlos a los dos";
+	gotoXY(PREGUNTA2POSX, PREGUNTAPOSY);
+    cout << "Hacer que peleen a muerte";
+	
+	//Control
+	int o = control();
+	if (o == 2)
+	{
+		contador++;
+		gotoXY(15, 21);
+    	cout << "Excelente, los trabajadores deben resolver sus diferencias!";
+	} else {
+		gotoXY(15, 21);
+    	cout << "Incorrecto, y ahora te esperan dos demandas...";
+	}
+	
 }
